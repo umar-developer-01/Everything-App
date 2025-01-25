6b8e8311e6d5f6ba0a02d2c038be4105a026f212
umar-developer-01 <umarkhandev9@gmail.com>
2025-01-25 17:28:47 +0530

    Added buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,54 @@
 import Image from "next/image";
-
+import XLogo from "../public/xlogo.jpg";
+import GoogleLogo from "../public/google.png";
+import AppleLogo from "../public/apple.png";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            It's Everything app Baby{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex gap-4 h-screen w-screen bg-black">
+      <div className="flex-1 flex justify-center items-center">
+        <Image src={XLogo} alt="Picture of the x" height={500} width={500} />
+      </div>
+      <div className="flex-1 flex justify-center text-white">
+        <div>
+          <div className="text-6xl font-medium pt-32 ">Happening Now</div>
+          <div className="text-2xl pt-10">Join Today.</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="py-5 flex flex-col items-start gap-3">
+            <button className="bg-white text-black text-sm font-thin px-16 py-1 rounded-full flex items-center w-72 h-8">
+              <Image src={GoogleLogo} alt="Google" height={25} width={25} />
+              Signup with Google
+            </button>
+
+            <button className="bg-white text-black text-sm font-thin px-16 py-1 rounded-full flex items-center w-72 h-8">
+              <Image src={AppleLogo} alt="Google" height={25} width={25} />
+              Signup with Apple
+            </button>
+
+            <div className="mx-4 w-64 flex items-center gap-2">
+              <div className="h-[0.1px] w-32 bg-white"></div>
+              <div className="text-sm">or</div>
+              <div className="h-[0.1px] w-32 bg-white"></div>
+            </div>
+            <button>Create Account</button>
+            <div>
+              By signing up, you agree to the Terms of Service and Privacy
+              Policy, including Cookie Use.
+            </div>
+          </div>
+
+          <div>Already have an account?</div>
+          <button>Signin</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
+
+// <div className="flex-1 h-full w-full">
+// <Image
+//   src={XLogo}
+//   alt="Picture of the author"
+//   layout="responsive" /* Ensures the image scales properly */
+//   objectFit="contain" /* Adjusts how the image fits inside its container */
+// />
+// </div>
