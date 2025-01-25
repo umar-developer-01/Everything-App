5a7197537b505194d4a07f39f4559e4664f694e0
umar-developer-01 <umarkhandev9@gmail.com>
2025-01-25 19:24:25 +0530

    Changes in signin account

--- components/signinAccount.jsx
+++ components/signinAccount.jsx
@@ -5,9 +5,13 @@ const SigninAccount = () => {
   const handleSigninAccount = () => {
     console.log("I am here in Signin ");
   };
+
   return (
     <>
-      <button onClick={handleSigninAccount} className="text-blue-500 bg-black text-sm border-[1px] font-thin px-16 py-1 rounded-full flex justify-center items-center w-72 h-8">
+      <button
+        onClick={handleSigninAccount}
+        className="text-blue-500 bg-black text-sm border-[1px] font-thin px-16 py-1 rounded-full flex justify-center items-center w-72 h-8"
+      >
         Signin
       </button>
     </>
