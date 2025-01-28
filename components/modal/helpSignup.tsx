c743e16fed903287427eba4b0034ac72f0bc4c39
umar-developer-01 <umarkhandev9@gmail.com>
2025-01-28 23:59:30 +0530

    Reomve backdrop

--- components/modal/helpSignup.tsx
+++ components/modal/helpSignup.tsx
@@ -1,7 +1,12 @@
 "use client";
 import Image from "next/image";
 
-import { Dialog, DialogPanel } from "@headlessui/react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
 import XLogo from "../../public/xlogo.jpg";
 
 import { XMarkIcon } from "@heroicons/react/24/outline";
@@ -73,19 +78,13 @@ const Signup = ({
     <>
       <Dialog
         open={open}
-        onClose={() => {}} // Prevent auto-close on backdrop click
+        onClose={() => setOpen(false)}
         className="relative z-10"
       >
-        <div
-          className="fixed inset-0 bg-gray-500/75"
-          onMouseDown={(e) => e.stopPropagation()} // Prevent clicks on backdrop from propagating
-        />
+        <DialogBackdrop className="fixed inset-0 bg-gray-500/75" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <DialogPanel
-              className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
-              onMouseDown={(e) => e.stopPropagation()} // Prevent clicks inside the modal from closing it
-            >
+            <DialogPanel className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
               <div className="bg-black pb-10">
                 {/* Heading and Cross */}
                 <div className="relative flex justify-center">
@@ -205,3 +204,53 @@ const Signup = ({
 };
 
 export default Signup;
+
+{
+  /* <div className="bg-black px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+              
+
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    {/* <DialogTitle
+                      as="h3"
+                      className="text-base font-semibold text-gray-900"
+                    > */
+}
+{
+  /* Deactivate account */
+}
+
+{
+  /* </DialogTitle> */
+}
+{
+  /* <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Are you sure you want to deactivate your account? All of
+                        your data will be permanently removed. This action
+                        cannot be undone.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div> */
+}
+{
+  /* <div className="bg-black px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button
+                  type="button"
+                  onClick={() => setOpen(false)}
+                  className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
+                >
+                  Deactivate
+                </button>
+                <button
+                  type="button"
+                  data-autofocus
+                  onClick={() => setOpen(false)}
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                >
+                  Cancel
+                </button>
+              </div> */
+}
