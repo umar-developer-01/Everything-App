38a9631aefc7c8fec1a5656579a8559bf79de77f
umar-developer-01 <umarkhandev9@gmail.com>
2025-01-28 23:40:59 +0530

    Changes in signup

--- components/modal/signup.tsx
+++ components/modal/signup.tsx
@@ -84,7 +84,7 @@ const Signup = ({
         <DialogBackdrop className="fixed inset-0 bg-gray-500/75" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <DialogPanel className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
               <div className="bg-black">
                 {/* Heading and Cross */}
                 <div className="relative flex justify-center">
@@ -189,7 +189,7 @@ const Signup = ({
                     </div>
                   </div>
                 </div>
-                <div className="my-4 flex justify-center">
+                <div className="my-10 flex justify-center">
                   <button className="bg-white text-black text-sm font-thin px-16 py-1 rounded-full  w-72 h-8 ">
                     Next
                   </button>
