e88a3e2a98bbd085931b4b30e3da02b2793c9219
umar-developer-01 <umarkhandev9@gmail.com>
2025-02-02 23:40:09 +0530

    Changes in signup

--- components/modal/signup.tsx
+++ components/modal/signup.tsx
@@ -1,34 +1,37 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useTheme } from "@/components/context/index";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import XLogo from "@/public/xlogo.jpg";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-const Signup = ({
-  open,
-}: 
-{
-  open: boolean;
-
-}) => {
+const Signup = ({ open }: { open: boolean }) => {
+  const router = useRouter();
   const { handlePopup } = useTheme();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [showPassword, setshowPassword] = useState<boolean>(false);
+  const [password, setpassword] = useState<string>("");
 
-  const [selectedDay, setSelectedDay] = useState("");
-  const [selectedMonth, setSelectedMonth] = useState("");
-  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedDay, setSelectedDay] = useState<string>("");
+  const [selectedMonth, setSelectedMonth] = useState<string>("");
+  const [selectedYear, setSelectedYear] = useState<string>("");
 
   const [daysInMonth, setDaysInMonth] = useState<number[]>([]); // Store the list of days
 
   const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
+    setName(e.target.value || "");
   };
 
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    setEmail(e.target.value || "");
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setpassword(e.target.value || "");
   };
 
   // Helper function to calculate the number of days in a month
@@ -59,13 +62,12 @@ const Signup = ({
     const response = await fetch("/api/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password: "1234444", name }),
+      body: JSON.stringify({ email, password, name }),
     });
-    console.log("this is the response", response);
     if (response.ok) {
-      alert("Signup successful!");
+      router.push("/dashboard");
     } else {
-      alert("Signup failed");
+      setError("Signup failed")
     }
   };
 
@@ -127,6 +129,56 @@ const Signup = ({
                       placeholder="Email"
                       className="input-box"
                     />
+                           <div className="relative">
+                            <input
+                              value={password}
+                              onChange={handlePassword}
+                              placeholder="Password"
+                              type={showPassword ? "text" : "password"}
+                              className="input-box w-full"
+                            />
+
+                            {showPassword && (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-6 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                                onClick={() => setshowPassword(!showPassword)}
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                                />
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                />
+                              </svg>
+                            )}
+
+                            {!showPassword && (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-6 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                                onClick={() => setshowPassword(!showPassword)}
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                                />
+                              </svg>
+                            )}
+                          </div>
                   </div>
 
                   <div className="mt-6">
@@ -201,6 +253,7 @@ const Signup = ({
                         </select>
                       </div>
                     </div>
+                    {error && <p className="text-red-500 mt-4">{error}</p>}
                   </div>
                 </div>
                 <div className="mt-10 flex justify-center">
