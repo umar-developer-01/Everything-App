5e4367db57caf4702162744553145f75c88b5844
umar-developer-01 <umarkhandev9@gmail.com>
2025-04-27 23:24:57 +0530

    Added Followinf For you

--- components/main/page.tsx
+++ components/main/page.tsx
@@ -2,7 +2,16 @@
 
 const MainContent = () => {
   return (
-    <div className="flex flex-col text-white">MainContent</div>
+    <div className="flex flex-col text-white">
+      <div className="flex cursor-pointer">
+         <div className="basis-1/3 flex justify-center items-center hover:bg-gray-800 p-3">For you</div>
+         <div className="basis-1/3 flex justify-center items-center hover:bg-gray-800 p-3">Following</div>
+         <div className="basis-1/3 flex justify-center items-center hover:bg-gray-800 p-3">React</div>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
