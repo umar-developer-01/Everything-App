6e40b713d16b35b0c52adf7c9c85c52f68fc6b4b
umar-developer-01 <umarkhandev9@gmail.com>
2025-04-28 23:27:37 +0530

    Added fonts

--- components/main/body.tsx
+++ components/main/body.tsx
@@ -10,7 +10,7 @@ const Body = () => {
           <p className="text-gray-500 text-xl my-2">What's happening?</p>
         </div>
 
-        <div className="mt-2 pl-16">
+        <div className="mt-2 pl-16 flex justify-between">
           <div className="flex gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
@@ -43,8 +43,9 @@ const Body = () => {
               <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
             </svg>
-
-
+          </div>
+          <div>
+            Post
           </div>
         </div>
       </div>
