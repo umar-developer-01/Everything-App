5a15e90a5ba337270a4cbce4674bc56753f0722d
umar-developer-01 <umarkhandev9@gmail.com>
2025-05-11 00:31:20 +0530

    added the ref

--- components/sidebars/popup.tsx
+++ components/sidebars/popup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,10 +7,30 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   return (
-    <div className="fixed mt-[48px] w-[320px] h-[330px] flex z-50 bg-black rounded-lg shadow-lg  py-2 border border-gray-500 ">
+    <div className="fixed mt-[48px] w-[320px] h-[330px] flex z-50 bg-black rounded-lg shadow-lg  py-2 border border-gray-500"  ref={modalRef}>
       {children}
     </div>
   );
